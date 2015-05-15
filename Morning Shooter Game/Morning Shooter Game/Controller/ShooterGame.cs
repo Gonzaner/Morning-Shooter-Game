3c39b1f0648954afe5257ee2ada7bed18ff6cce2
Nery Gonzalez <mjmssucks@gmail.com>
2015-05-15 07:18:26 -0600

    Latest Commit

--- Morning Shooter Game/Morning Shooter Game/Controller/ShooterGame.cs
+++ Morning Shooter Game/Morning Shooter Game/Controller/ShooterGame.cs
@@ -9,6 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using Morning_Shooter_Game.Model;
+using Morning_Shooter_Game.View;
 
 namespace Morning_Shooter_Game.Controller
 {
@@ -32,8 +33,48 @@ namespace Morning_Shooter_Game.Controller
 
         // A movement speed for the player
         float playerMoveSpeed;
-        // Set a constant player move speed
-        
+        //Image used to display the static background
+        Texture2D mainBackground;
+
+        //Parallaxing Layers
+        ParallaxingBackground bgLayer1;
+        ParallaxingBackground bgLayer2;
+
+        // Enemies
+        Texture2D enemyTexture;
+        List<Enemy> enemies;
+
+        // The rate at which the enemies appear
+        TimeSpan enemySpawnTime;
+        TimeSpan previousSpawnTime;
+
+        // A random number generator
+        Random random;
+
+        //projectiles 
+        Texture2D projectileTexture;
+        List<Projectile> projectiles;
+        Texture2D burstTexture;
+       // List<Burst> burst1;
+       // List<Burst> burst2;
+       // List<Burst> burst3;
+        // The rate of fire of the player laser
+        TimeSpan fireTime;
+        TimeSpan previousFireTime;
+        Texture2D explosionTexture;
+        List<Animation> explosions;
+        // The sound that is played when a laser is fired
+        SoundEffect laserSound;
+
+        // The sound used when the player or an enemy dies
+        SoundEffect explosionSound;
+
+        // The music played during gameplay
+        Song gameplayMusic;
+        //Number that holds the player score
+        int score;
+        // The font used to display UI elements
+        SpriteFont font;
         public ShooterGame()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -48,12 +89,36 @@ namespace Morning_Shooter_Game.Controller
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+           // burst1 = new List<Burst>();
+           // burst2 = new List<Burst>();
+           // burst3 = new List<Burst>();
+            projectiles = new List<Projectile>();
+            explosions = new List<Animation>();
+            //Set player's score to zero
+            score = 0;
+            // Set the laser to fire every quarter second
+            fireTime = TimeSpan.FromSeconds(.15f);
 
+            bgLayer1 = new ParallaxingBackground();
+            bgLayer2 = new ParallaxingBackground();
             // Initialize the player class
             player = new Player();
             // Set a constant player move speed
             playerMoveSpeed = 8.0f;
+            // Initialize the enemies list
+            enemies = new List<Enemy>();
+
+            // Set the time keepers to zero
+            previousSpawnTime = TimeSpan.Zero;
+
+            // Used to determine how fast enemy respawns
+            enemySpawnTime = TimeSpan.FromSeconds(1.0f);
+
+            // Initialize our random number generator
+            random = new Random();
+            
+
+            
             base.Initialize();
         }
 
@@ -66,9 +131,31 @@ namespace Morning_Shooter_Game.Controller
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // Load the player resources 
-            Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
-            player.Initialize(Content.Load<Texture2D>("Images/player"), playerPosition);
+            // Load the player resources
+            Animation playerAnimation = new Animation();
+            Texture2D playerTexture = Content.Load<Texture2D>("Images/shipAnimation");
+            playerAnimation.Initialize(playerTexture, Vector2.Zero, 115, 69, 8, 30, Color.White, 1f, true);
+
+            Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y
+            + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+            player.Initialize(playerAnimation, playerPosition);
+            // Load the parallaxing background
+            bgLayer1.Initialize(Content, "Images/bgLayer1", GraphicsDevice.Viewport.Width, -1);
+            bgLayer2.Initialize(Content, "Images/bgLayer2", GraphicsDevice.Viewport.Width, -2);
+            enemyTexture = Content.Load<Texture2D>("Images/mineAnimation");
+            projectileTexture = Content.Load<Texture2D>("Images/laser"); 
+            explosionTexture = Content.Load<Texture2D>("Images/explosion");
+            // Load the music
+            gameplayMusic = Content.Load<Song>("sounds/gameMusic");
+
+            // Load the laser and explosion sound effect
+            laserSound = Content.Load<SoundEffect>("sounds/laserFire");
+            explosionSound = Content.Load<SoundEffect>("sounds/explosion");
+            // Load the score font
+            font = Content.Load<SpriteFont>("Fonts/gameFont");
+            // Start the music right away
+            PlayMusic(gameplayMusic);
+            mainBackground = Content.Load<Texture2D>("Images/mainbackground");
 
             
         }
@@ -106,11 +193,188 @@ namespace Morning_Shooter_Game.Controller
 
             //Update the player
             UpdatePlayer(gameTime);
-            
+
+            // Update the parallaxing background
+            bgLayer1.Update();
+            bgLayer2.Update();
+            UpdateEnemies(gameTime);
+            // Update the collision
+            UpdateCollision();
+            // Update the projectiles
+            UpdateProjectiles();
+            // Update the explosions
+            UpdateExplosions(gameTime);
             base.Update(gameTime);
         }
+
+        private void AddExplosion(Vector2 position)
+        {
+            Animation explosion = new Animation();
+            explosion.Initialize(explosionTexture, position, 134, 134, 12, 45, Color.White, 1f, false);
+            explosions.Add(explosion);
+        }
+        private void UpdateExplosions(GameTime gameTime)
+        {
+            for (int i = explosions.Count - 1; i >= 0; i--)
+            {
+                explosions[i].Update(gameTime);
+                if (explosions[i].Active == false)
+                {
+                    explosions.RemoveAt(i);
+                }
+            }
+        }
+        private void AddProjectile(Vector2 position)
+        {
+            Projectile projectile = new Projectile();
+            projectile.Initialize(GraphicsDevice.Viewport, projectileTexture, position);
+            projectiles.Add(projectile);
+        }
+        private void UpdateProjectiles()
+        {
+            // Update the Projectiles
+            for (int i = projectiles.Count - 1; i >= 0; i--)
+            {
+                projectiles[i].Update();
+
+                if (projectiles[i].Active == false)
+                {
+                    projectiles.RemoveAt(i);
+                }
+            }
+        }
+        private void PlayMusic(Song song)
+        {
+            // Due to the way the MediaPlayer plays music,
+            // we have to catch the exception. Music will play when the game is not tethered
+            try
+            {
+                // Play the music
+                MediaPlayer.Play(song);
+
+                // Loop the currently playing song
+                MediaPlayer.IsRepeating = true;
+            }
+            catch { }
+        }
+        private void UpdateCollision()
+        {
+            // Use the Rectangle's built-in intersect function to 
+            // determine if two objects are overlapping
+            Rectangle rectangle1;
+            Rectangle rectangle2;
+
+            // Only create the rectangle once for the player
+            rectangle1 = new Rectangle((int)player.Position.X,
+            (int)player.Position.Y,
+            player.Width,
+            player.Height);
+
+            // Do the collision between the player and the enemies
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                rectangle2 = new Rectangle((int)enemies[i].Position.X,
+                (int)enemies[i].Position.Y,
+                enemies[i].Width,
+                enemies[i].Height);
+
+                // Determine if the two objects collided with each
+                // other
+                if (rectangle1.Intersects(rectangle2))
+                {
+                    // Subtract the health from the player based on
+                    // the enemy damage
+                    player.Health -= enemies[i].Damage;
+
+                    // Since the enemy collided with the player
+                    // destroy it
+                    enemies[i].Health = 0;
+
+                    // If the player health is less than zero we died
+                    if (player.Health <= 0)
+                        player.Active = false;
+                }
+            }
+            // Projectile vs Enemy Collision
+            for (int i = 0; i < projectiles.Count; i++)
+            {
+                for (int j = 0; j < enemies.Count; j++)
+                {
+                    // Create the rectangles we need to determine if we collided with each other
+                    rectangle1 = new Rectangle((int)projectiles[i].Position.X -
+                    projectiles[i].Width / 2, (int)projectiles[i].Position.Y -
+                    projectiles[i].Height / 2, projectiles[i].Width, projectiles[i].Height);
+
+                    rectangle2 = new Rectangle((int)enemies[j].Position.X - enemies[j].Width / 2,
+                    (int)enemies[j].Position.Y - enemies[j].Height / 2,
+                    enemies[j].Width, enemies[j].Height);
+
+                    // Determine if the two objects collided with each other
+                    if (rectangle1.Intersects(rectangle2))
+                    {
+                        enemies[j].Health -= projectiles[i].Damage;
+                        projectiles[i].Active = false;
+                    }
+                }
+            }
+        }
+
+        private void AddEnemy()
+        {
+            // Create the animation object
+            Animation enemyAnimation = new Animation();
+
+            // Initialize the animation with the correct animation information
+            enemyAnimation.Initialize(enemyTexture, Vector2.Zero, 47, 61, 8, 30, Color.White, 1f, true);
+
+            // Randomly generate the position of the enemy
+            Vector2 position = new Vector2(GraphicsDevice.Viewport.Width + enemyTexture.Width / 2, random.Next(100, GraphicsDevice.Viewport.Height - 100));
+
+            // Create an enemy
+            Enemy enemy = new Enemy();
+
+            // Initialize the enemy
+            enemy.Initialize(enemyAnimation, position);
+
+            // Add the enemy to the active enemies list
+            enemies.Add(enemy);
+        }
+        private void UpdateEnemies(GameTime gameTime)
+        {
+            // Spawn a new enemy enemy every 1.5 seconds
+            if (gameTime.TotalGameTime - previousSpawnTime > enemySpawnTime)
+            {
+                previousSpawnTime = gameTime.TotalGameTime;
+
+                // Add an Enemy
+                AddEnemy();
+            }
+
+            // Update the Enemies
+            for (int i = enemies.Count - 1; i >= 0; i--)
+            {
+                enemies[i].Update(gameTime);
+
+                if (enemies[i].Active == false)
+                {
+                    // If not active and health <= 0
+                    if (enemies[i].Health <= 0)
+                    {
+                        // Add an explosion
+                        AddExplosion(enemies[i].Position);
+                        // Play the explosion sound
+                        explosionSound.Play();
+                        //Add to the player's score
+                        score += enemies[i].Value;
+                    }
+                    enemies.RemoveAt(i);
+                }
+            }
+        }
+
         private void UpdatePlayer(GameTime gameTime)
         {
+            player.Update(gameTime);
 
             // Get Thumbstick Controls
             player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
@@ -141,6 +405,23 @@ namespace Morning_Shooter_Game.Controller
             // Make sure that the player does not go out of bounds
             player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
             player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+            // Fire only every interval we set as the fireTime
+            if (gameTime.TotalGameTime - previousFireTime > fireTime)
+            {
+                // Reset our current time
+                previousFireTime = gameTime.TotalGameTime;
+
+                // Add the projectile, but add it to the front and center of the player
+                AddProjectile(player.Position + new Vector2(player.Width / 2, 0));
+                // Play the laser sound
+                laserSound.Play();
+            }
+            // reset score if player health goes to zero
+            if (player.Health <= 0)
+            {
+                player.Health = 100;
+                score = 0;
+            }
         }
 
         /// <summary>
@@ -155,10 +436,33 @@ namespace Morning_Shooter_Game.Controller
             spriteBatch.Begin();
 
             // All Drawing happends AFTER .begins() is called!
+            
+            spriteBatch.Draw(mainBackground, Vector2.Zero, Color.White);
 
+            // Draw the moving background
+            bgLayer1.Draw(spriteBatch);
+            bgLayer2.Draw(spriteBatch);
+            // Draw the Enemies
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                enemies[i].Draw(spriteBatch);
+            }
+            // Draw the Projectiles
+            for (int i = 0; i < projectiles.Count; i++)
+            {
+                projectiles[i].Draw(spriteBatch);
+            }
             // Draw the Player
             player.Draw(spriteBatch);
-
+            // Draw the explosions
+            for (int i = 0; i < explosions.Count; i++)
+            {
+                explosions[i].Draw(spriteBatch);
+            }
+            // Draw the score
+            spriteBatch.DrawString(font, "score: " + score, new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y), Color.White);
+            // Draw the player health
+            spriteBatch.DrawString(font, "health: " + player.Health, new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + 30), Color.White);
             // Stop drawing
             spriteBatch.End();
 
--- Morning Shooter Game/Morning Shooter Game/Model/Player.cs
+++ Morning Shooter Game/Morning Shooter Game/Model/Player.cs
@@ -1,19 +1,13 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-
+using Morning_Shooter_Game.View;
 namespace Morning_Shooter_Game.Model
 {
   public class Player
     {
         // Animation representing the player
-        private Texture2D playerTexture;
-
-        public Texture2D PlayerTexture
-        {
-            get { return playerTexture; }
-            set { playerTexture = value; }
-        }
+        public Animation PlayerAnimation;
 
         // Position of the Player relative to the upper left side of the screen
         public Vector2 Position;
@@ -38,36 +32,39 @@ namespace Morning_Shooter_Game.Model
         // Get the width of the player ship
         public int Width
         {
-            get { return PlayerTexture.Width; }
+            get { return PlayerAnimation.FrameWidth; }
         }
 
         // Get the height of the player ship
         public int Height
         {
-            get { return PlayerTexture.Height; }
+            get { return PlayerAnimation.FrameHeight; }
         }
 
-        public void Initialize(Texture2D playerTexture, Vector2 position)
+        public void Initialize(Animation animation, Vector2 position)
         {
-            this.playerTexture = playerTexture;
+            PlayerAnimation = animation;
 
             // Set the starting position of the player around the middle of the screen and to the back
             Position = position;
 
             // Set the player to be active
-            active = true;
+            Active = true;
 
             // Set the player health
             Health = 100;
         }
 
-        public void Update()
-        {
-        }
+       // Update the player animation
+public void Update(GameTime gameTime)
+{
+PlayerAnimation.Position = Position;
+PlayerAnimation.Update(gameTime);
+}
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+            PlayerAnimation.Draw(spriteBatch);
         }
 
     }
